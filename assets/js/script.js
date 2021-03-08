92100a5f530f7eab5035fe004e82026622160f51
Daniel Lucas <danomiterock@yahoo.com>
2021-03-08 15:03:39 -0700

    create initial documents

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var api_key = "33c4d45a4597dc2f2025c1d244be92c6";
+
+var currentWeatherURL = `api.openweathermap.org/data/2.5/weather?q=${city name},${state code}&appid=${API key}`
+
+fetch(currentWeatherURL)
+    .then((data) => data.json{))
+    .then(function{weather}{})
