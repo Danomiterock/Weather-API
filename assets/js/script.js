da35875f1e62ddd2cf9fc8d2bdeb1ff9d2bc51f4
Daniel Lucas <danomiterock@yahoo.com>
2021-03-09 10:21:28 -0700

    add css and js links

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,20 @@ fetch(currentWeatherURL)
 
     })
 
-    //Starting point
-//     var city = "Temecula";
-// var currentWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-// fetch(currentWeatherUrl)
-//   .then((data) => data.json())
-//   .then(function (weather) {
-//     console.log(weather);
-//     var lat = weather.coord.lat;
-//     var lon = weather.coord.lon;
-//     var onecallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`;
-//     fetch(onecallURL)
-//       .then((data) => data.json())
-//       .then(function (oneCallData) {
-//         //   oneCallData has all the information that we need
-//         console.log(oneCallData);
-//       });
-//   });
+Starting point
+    var city = "Temecula";
+var currentWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+fetch(currentWeatherUrl)
+  .then((data) => data.json())
+  .then(function (weather) {
+    console.log(weather);
+    var lat = weather.coord.lat;
+    var lon = weather.coord.lon;
+    var onecallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${api_key}`;
+    fetch(onecallURL)
+      .then((data) => data.json())
+      .then(function (oneCallData) {
+        //   oneCallData has all the information that we need
+        console.log(oneCallData);
+      });
+  });
